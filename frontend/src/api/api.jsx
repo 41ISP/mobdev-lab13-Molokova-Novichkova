7b4765c3b26a41905baa35f66fa6078c87b01f8c
MargaritaMolokova <shuashl@mail.ru>
2025-10-10 08:05:05 +0000

    ошибки исправлены

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,4 +1,5 @@
 export const registerUser = async (user) => {
+    try {
     const req = await fetch ("https://kitek.ktkv.dev/feedback/api/auth/register",{
         method: "POST",
         body: JSON.stringify(user),
@@ -7,5 +8,32 @@ export const registerUser = async (user) => {
         }
     })
     const res = await req.json()
+    if (!res.success) {
+        throw new Error (res.error)
+    }
     return res
+} catch {err} {
+    console.error(err)
+    throw new Error (err)
+}
+}
+
+export const loginUser = async (user) => {
+    try {
+    const req = await fetch ("https://kitek.ktkv.dev/feedback/api/auth/login",{
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const res = await req.json()
+    if (!res.success) {
+        throw new Error (res.error)
+    }
+    return res
+} catch {err} {
+    console.error(err)
+    throw new Error (err)
+}
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,17 +2,31 @@ import { Link } from "react-router-dom"
 import Button from "../components/Button"
 import Input from "../components/Input"
 import { useState } from "react"
+import { registerUser } from "../api/api"
 
 const SignUp = () => {
     const [error, setError] = useState("")
 
-    const handleSubmit =(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         setError("")
         console.log(e.target.username.value)
         if (e.target.password.value != e.target.password2.value) {
             setError("Пароли не совпадают");
             return;
+        }
+        const user = {
+            username: e.target.username.value,
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        try {
+            const data = await registerUser(user)
+            console.log(data);
+        }
+        catch (err) {
+            console.error(err)
+            setError(err.message)
         }
     }
     return (
